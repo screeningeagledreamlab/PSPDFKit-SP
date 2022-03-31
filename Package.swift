3bcded7639669f4573f2ff44e6ce051a29a391af
bondxf <fengxubond@gmail.com>
2022-03-31 17:18:17 +0800

    Update checksum for the 11.2.4 binary frameworks

--- Package.swift
+++ Package.swift
@@ -13,10 +13,10 @@ let package = Package(
         .binaryTarget(
             name: "PSPDFKit",
             url: "https://eagleinspect-ios-dependencies.s3.ap-southeast-1.amazonaws.com/swift-package-manager/pspdfkit/xcframework/11.2.4.zip",
-            checksum: "486f86a5ff9550c49b42348357f82e148edfa8143e7e3bcbee1c5d40b9746940"),
+            checksum: "5cd9934f16044c3d69a05b79f4470bf6eb6166937e19ae30a61fddc35c657c91"),
         .binaryTarget(
             name: "PSPDFKitUI",
             url: "https://eagleinspect-ios-dependencies.s3.ap-southeast-1.amazonaws.com/swift-package-manager/pspdfkitui/xcframework/11.2.4.zip",
-            checksum: "f77ad43f86a7d7eafe519d46207fa36a0038939ec1801731716953bf557e7d77"),
+            checksum: "ef0ca8d6e2bf5fa21b8fe536bc8fbcfcf274d647630e7fba69856ec23d12bec6"),
     ]
 )
