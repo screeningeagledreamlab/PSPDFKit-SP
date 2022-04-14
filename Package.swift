8d1c1ac01133febdff82dffc111065a2b172e225
Junfeng Hu <junfeng.hu@screeningeagle.com>
2022-04-14 12:21:38 +0800

    Change to use internal mirror

--- Package.swift
+++ Package.swift
@@ -12,11 +12,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PSPDFKit",
-            url: "https://eagleinspect-ios-dependencies.s3.ap-southeast-1.amazonaws.com/swift-package-manager/pspdfkit/xcframework/11.2.4.zip",
+            url: "https://frameworks.tectusdreamlab.com/frameworks/PSPDFKit/v11.2.4/PSPDFKit.xcframework.zip",
             checksum: "5cd9934f16044c3d69a05b79f4470bf6eb6166937e19ae30a61fddc35c657c91"),
         .binaryTarget(
             name: "PSPDFKitUI",
-            url: "https://eagleinspect-ios-dependencies.s3.ap-southeast-1.amazonaws.com/swift-package-manager/pspdfkitui/xcframework/11.2.4.zip",
+            url: "https://frameworks.tectusdreamlab.com/frameworks/PSPDFKitUI/v11.2.4/PSPDFKitUI.xcframework.zip",
             checksum: "ef0ca8d6e2bf5fa21b8fe536bc8fbcfcf274d647630e7fba69856ec23d12bec6"),
     ]
 )
